f456d8a17fdaf9326adbe4ad33251913d7f0160a
azmkn <dog.galaxy.2020@gmail.com>
2025-05-29 07:51:09 +0000

    いったん

---
+++ prime_count.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef long long ll;
+
+int main(){
+
+  auto start = high_resolution_clock::now();
+
+  ll l, r, p ,c ,m;
+  double e = 2.718281828459045;
+  string s = "";
+  cin >> c;
+  cin >> l >> r;
+  cout << fixed << setprecision(15) << endl;
+  vector<ll> vec(3);
+  vec[0] = 0;
+  vec[1] = 4;
+  vec[2] = 6;
+  for (ll p1 = l / 10; p1 <= r / 10; p1++){
+
+    for(const ll& ao : vec){
+
+      p = p1 * 10 + ao;
+
+      if (l>p || p>r){
+
+        continue;
+
+      }
+
+      ll n = p * p + 1;
+      m = n;
+
+      bool torf = true;
+
+      for (ll i = 5; i <= p ; i += 4){
+
+        if (i == m){
+
+          torf = false;
+          break;
+
+        }
+
+        if (n % i == 0){
+
+          torf = false;
+
+          break;
+
+        }
+
+      }
+      if (torf) c++;
+      if (p%1000==0){
+        double lo=(e*p)/(4*(log(p)-1.08366));
+
+        /*cout << "log: " << lo << endl;
+        cout << "n: " << p << " count: " << c << endl;
+        
+        cout << "count / log = "*/cout << c/lo << endl;
+      }
+
+    }
+
+  }
+
+  auto end = high_resolution_clock::now();
+  
+  auto duration = duration_cast<milliseconds>(end - start).count();
+  cout << "Execution Time: " << duration << "ms" << endl;
+
+}
